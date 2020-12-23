06856799bce6786857184500b5736dff6bf71772
Coderzzz <hashirk966@gmail.com>
2020-12-23 22:49:49 +0500

    AddToCart

---
+++ app.js
@@ -0,0 +1,36 @@
+const car_cards = document.querySelector(".car_cards");
+const cart_products = document.querySelector(".cart_products");
+const cart_buttons = document.querySelectorAll(".cart_button");
+
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      let products = data.Items;
+      products = products.map((Items) => {
+        const { title, price } = Items.fields;
+        const { id } = Items.sys;
+        const image = Items.fields.image.url;
+        return { title, price, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("Products", JSON.stringify(products));
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const products = new Products();
+
+  products.getProducts().then((data) => {
+    Storage.saveProducts(data);
+  });
+});
---
+++ cart.js
@@ -0,0 +1,30 @@
+const cart_list = document.querySelector(".cart_products");
+
+const product = JSON.parse(localStorage.getItem("Products"));
+
+let result = "";
+product.forEach((element) => {
+  result += `
+      <li class="cart_product_items">
+      <div class="cart_card">
+        <div class="cart_card_image_about_info">
+          <div class="cart_card_image">
+            <figure>
+              <img src=${element.image} alt="" />
+            </figure>
+          </div>
+
+          <div class="cart_about_info">
+            <h1>${element.price}</h1>
+            <p>${element.title}</p>
+          </div>
+        </div>
+
+        <div class="remove_btn">
+          <button>Remove</button>
+        </div>
+      </div>
+    </li>
+      `;
+});
+cart_list.innerHTML = result;
